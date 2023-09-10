c5bb4fd7ce39d6974f26c407532701b10f5b6d5f
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-09-10 20:46:03 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { inject } from '@vercel/analytics';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+inject();
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
